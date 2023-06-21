5069b3e944ee8a7828873cebdd7b1f2ccd8db24d
Samie Bee <124081765+samiebee43@users.noreply.github.com>
2023-06-21 11:47:54 -0400

    Update client.py
    added description

--- client.py
+++ client.py
@@ -1,3 +1,8 @@
+"""
+Set integration time to 1000ms
+Acquire spectrum
+Print output to terminal
+"""
 import TinyWP
 print("Waiting for device...")
 while not TinyWP.get_devices():
@@ -7,4 +12,3 @@ print("Found Wasatch Device.", type(device))
 TinyWP.init(device)
 TinyWP.set_integration_time_ms(device, 1000)
 print(TinyWP.get_spectrum(device))
-
