d93781d57bd82cb902d76c74713841b95e382e7d
Samie Bee <sbee@wasatchphotonics.com>
2023-06-23 10:38:42 -0400

    simplify test_window.py

--- test_window.py
+++ test_window.py
@@ -1,19 +1,12 @@
-import PySimpleGUI as sg                        # Part 1 - The import
+import PySimpleGUI as sg                       
 
-# Define the window's contents
-layout = [  [sg.Text("What's your name?")],     # Part 2 - The Layout
-            [sg.Input()],
+layout = [  [sg.Text("This is a test window.")],    
             [sg.Button('Ok')] ]
 
-# Create the window
-window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
+window = sg.Window('Test Window', layout)     
                                                 
-# Display and interact with the Window
-event, values = window.read()                   # Part 4 - Event loop or Window.read call
+event, values = window.read()                  
+print(event, values)
 
-# Do something with the information gathered
-print('Hello', values[0], "! Thanks for trying PySimpleGUI")
-
-# Finish up by removing from the screen
-window.close()                                  # Part 5 - Close the Window
+window.close()                                 
 
